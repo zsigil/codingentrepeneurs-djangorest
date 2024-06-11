1502d6d3052abf6feb84dc57397f03e1fedf2b8d
Zsigil <chickpox@freemail.hu>
2024-06-11 11:40:54 +0200

    echoing back data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,22 @@
 # from django.shortcuts import render
+import json
 from django.http import JsonResponse
 # Create your views here.
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message":"Hi there, this is your django api response"})
+    # request -> HttpRequest -> Django
+    # print(dir(request))
+    # request.body - byte string of json data
+    print(request.GET)
+    body = request.body
+    # print(body)
+    # print(request.headers)
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,6 +4,6 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://127.0.0.1:8000/api/" #localhost
 
-get_response = requests.get(endpoint)
-print(get_response.json()["message"])
+get_response = requests.get(endpoint, params={"abc":123}, json={"query":"Hello world json"})
+print(get_response.json())
 print(get_response.status_code)
