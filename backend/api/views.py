0c6e04a510e5f47188f87061565e6d5ed44d15b6
Zsigil <chickpox@freemail.hu>
2024-06-11 10:52:53 +0200

    first api url

---
+++ backend/api/views.py
@@ -0,0 +1,7 @@
+# from django.shortcuts import render
+from django.http import JsonResponse
+# Create your views here.
+
+
+def api_home(request, *args, **kwargs):
+    return JsonResponse({"message":"Hi there, this is your django api response"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,8 +2,8 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://127.0.0.1:8000/" #localhost
+endpoint = "http://127.0.0.1:8000/api/" #localhost
 
 get_response = requests.get(endpoint)
-print(get_response.text)
+print(get_response.json()["message"])
 print(get_response.status_code)
