c5b7ecfb32fd324e1478d92977319106d375f230
Zsigil <chickpox@freemail.hu>
2024-06-17 17:27:06 +0200

    Model serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,13 +3,15 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from products.models import Product
+from products.serializers import ProductSeralizer
 # Create your views here.
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
+    if instance:
         #serialization: model_instance->python dictionary
-        data = model_to_dict(model_data, fields=['id', 'title'])
+        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
+        data = ProductSeralizer(instance).data
     return Response(data)
