ad7630decfa3bc8623f16be27edd9959979913d6
Zsigil <chickpox@freemail.hu>
2024-07-08 16:04:24 +0200

    customizing genericviewsets

--- backend/cfehome/routers.py
+++ backend/cfehome/routers.py
@@ -1,8 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from products.viewsets import ProductViewSet
+from products.viewsets import ProductGenericViewSet
 
 router = DefaultRouter()
-router.register('products-abc', ProductViewSet, basename="products")
+router.register('products-abc', ProductGenericViewSet, basename="products")
 
 
 urlpatterns = router.urls
--- backend/products/viewsets.py
+++ backend/products/viewsets.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 
 from .models import Product
 from .serializers import ProductSeralizer
@@ -16,3 +16,19 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSeralizer
     lookup_field = 'pk' #default
     
+
+class ProductGenericViewSet(mixins.ListModelMixin, 
+                            mixins.RetrieveModelMixin, 
+                            viewsets.GenericViewSet):
+    '''
+    get -> list -> queryset
+    get -> retrieve -> Product instance detail view
+    '''
+    queryset = Product.objects.all()
+    serializer_class = ProductSeralizer
+    lookup_field = 'pk' #default
+
+
+#to customize endpoints, use these in urls.py
+product_list_view = ProductGenericViewSet.as_view({'get':'list'})
+product_detail_view = ProductGenericViewSet.as_view({'get':'retrieve'})
